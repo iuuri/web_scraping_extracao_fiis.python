a3e20dc2c7dd73683e5a2dc504281b60592e0674
Iuri <iuri.souzasantos@gmail.com>
2024-05-01 11:44:23 -0300

    ajustes nos comentarios

--- app.py
+++ app.py
@@ -11,10 +11,14 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 url_site = 'https://fundamentus.com.br/fii_resultado.php'
 driver.get(url_site)
 
-#localizar e extrair tabela com Fiis
+#localiza tabela
 local_tabela = 'tabelaResultado'
 elemento_tabela = driver.find_element('id', local_tabela)
+
+#visualiza conteudo de dentro da tabela com o outerHTML
 html_tabela = elemento_tabela.get_attribute("outerHTML")
+
+#transforma o conteudo em string e converte "." para ","
 tabela = pd.read_html(str(html_tabela), thousands=".", decimal=",")[0]
 
 # Salvar os dados em um arquivo Excel
